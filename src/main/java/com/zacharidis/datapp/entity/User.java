27827750e9d4782597947812795d855b11b68683
Georgios Zacharidis <gzacharid@gmail.com>
2024-09-28 21:20:56 +0300

    User entity added without getters and setters. ToDo : Implement Lombok

--- src/main/java/com/zacharidis/datapp/entity/User.java
+++ src/main/java/com/zacharidis/datapp/entity/User.java
@@ -1,4 +1,61 @@
 package com.zacharidis.datapp.entity;
 
+
+import jakarta.persistence.*;
+import javax.management.relation.Role;
+import java.io.Serializable;
+import  java.util.Date;
+
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date createdAt;
+
+    public enum  Role {
+        ADMIN,DOCTOR,PATIENT
+    }
+
+
+    public User(Long id, String name, String email, String password, Role role, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
